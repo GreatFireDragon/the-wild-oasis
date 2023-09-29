885d8b11f3b274d1f0005a7a3962e5290a94cbcf
greatfiredragon <kulmike2002@gmail.com>
2023-09-29 15:07:19 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading>Wold Oasis</Heading>
+
+        <Button onClick={() => console.log("Check in")}>Check in</Button>
+        <Button onClick={() => console.log("Check out")}>Check out</Button>
+
+        <Input type="number" placeholder="number of guests" />
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
